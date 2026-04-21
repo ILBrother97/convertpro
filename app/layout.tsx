1a49fd974257d4858d94e3483a9a4484f3ef1314
ILBrother97 <abdiissack270@gmail.com>
2026-04-21 16:12:23 +0300

    SEO: Updated Google Search Console verification tag for new account

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" className={inter.variable}>
       <head>
-        <meta name="google-site-verification" content="xIxZxrVCSG1Wwh4lfnKMXa1D2k9FkUjBHvu_kM3PPto" />
+        <meta name="google-site-verification" content="YMFANnP5KMDz6gZqTgriiHSOR-6g2uaA5NAHaKHxpTM" />
         {isRealAdsense && (
           <Script
             async
