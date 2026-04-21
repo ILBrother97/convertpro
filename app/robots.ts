fab87e61f0abcc630a082759a89a201b960599b6
ILBrother97 <abdiissack270@gmail.com>
2026-04-21 16:22:38 +0300

    SEO: Added robots.ts to point Google directly to the sitemap

--- app/robots.ts
+++ app/robots.ts
@@ -2,7 +2,11 @@ import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: { userAgent: '*', allow: '/' },
-    sitemap: 'https://convertpro.io/sitemap.xml',
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: '/api/',
+    },
+    sitemap: 'https://convertpro-online.vercel.app/sitemap.xml',
   }
 }
